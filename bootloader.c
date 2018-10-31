45bbee7a5ca40e8cbfc36b602842701257e0a98f
Igor Bondarev <igor.bondarev@hotmail.com>
2018-10-31 20:39:47 +0300

    Preparing for switching to PM

--- bootloader.c
+++ bootloader.c
@@ -1,5 +1,58 @@
+#define GDT_FLAGS_SZ 2
+#define GDT_FLAGS_GR 3
+#define GDT_AB_AC 0
+#define GDT_AB_RW 1
+#define GDT_AB_DC 2
+#define GDT_AB_EX 3
+#define GDT_AB_PRIV 5
+#define GDT_AB_PR 7
+#define GDT_AB_MASK (1 << 4)
+
+struct gdt_entry {
+	unsigned int limit_low16 : 16;
+	unsigned int base_low16 : 16;
+	unsigned int base_mid8 : 8;
+	unsigned int access_byte: 8;
+	unsigned int limit_high4 : 4;
+	unsigned int flags : 4;
+	unsigned int base_high8 : 8;
+
+};
+
+struct gdt_entry gdt_entries[4];
+
+static void kmemset(void *dst, char num, unsigned int sz)
+{
+	char *mem = (char *)dst;
+
+	sz--;
+	while (sz >= 0) {
+		mem[sz] = num;
+		sz--;
+	}
+}
+
+static void set_gdt_entry(struct gdt_entry *ent, unsigned int base,
+			  unsigned int limit, unsigned char access_byte,
+			  unsigned char flags)
+{
+	ent->limit_low16 = limit & 0xFFFF;
+	ent->limit_high4 = (limit >> 16) & 0xF;
+	ent->base_low16 = base & 0xFFFF;
+	ent->base_mid8 = (base >> 16) & 0xFF;
+	ent->base_high8 = (base >> 24) & 0xFF;
+	ent->flags = flags & 0xF;
+	ent->access_byte = access_byte;
+}
+
 void kmain(void)
 {
+	kmemset(gdt_entries, 0, sizeof(gdt_entries));
+	/* null descriptor alread zeroed. */
+
+	// FIXME type & flags
+	set_gdt_entry(&gdt_entries[1], 0, 0xFFFF, 0, 0);
+
 	while (1)
 		;
 	return;
--- mbr.s
+++ mbr.s
@@ -5,17 +5,30 @@ segment .text
 global asm_main
 asm_main:
 	cli
+	; Disable NMI
+	in al, 0x70
+	or al, 0x80
+	out 0x70, al
 	xor ax, ax
+	; cs must be 0 already
 	mov ss, ax
 	mov ds, ax
 	mov es, ax
 	mov fs, ax
 	mov gs, ax
 	mov sp, 0x7C00 ; 30KB hole
+	; Read 2'nd bootloader
 	mov si, DAP
 	mov ah, 0x42
 	int 0x13
+	; Fast A20
+	in al, 0x92
+	or al, 2
+	out 0x92, al
+	; Enable PM
+	; ljmp 0x0008:0x8000
 	call 0x8000 ; _start() function, 0x7E00 + 0x200
+	jmp $
 
 segment .data
 DAP:
@@ -25,6 +38,6 @@ DAP:
 	dw 0x7E00 ; offset
 	dw 0 ; segment
 	dq 1 ; start sector
-	resb 466
+	resb 454
 	db 0x55
 	db 0xAA
